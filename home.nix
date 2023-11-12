b16ca19284f134bb31dca67f92dced8618adb4fb
muiga <muigask@gmail.com>
2023-11-12 07:35:23 +0300

    initial config 2

--- home.nix
+++ home.nix
@@ -8,43 +8,16 @@ let
 
 in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+
   home.username = "muiga";
   home.homeDirectory = "/home/muiga";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
-
-
     # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+ 
       firefox
       kate
       vscode
@@ -135,6 +108,7 @@ in
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
     enableCompletion = true;
     dotDir = ".config/zsh";
     shellAliases = myAliases;
@@ -143,20 +117,13 @@ in
     name = "powerlevel10k";
     src = pkgs.zsh-powerlevel10k;
     file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    }
-    # {
-    #  name = "powerlevel10k-config";
-    #  src = ./p10k-config;
-    #  file = "p10k.zsh";
-    # }
+    }  
     ];
     initExtra = '' 
       source ./.p10k.zsh
     '';
     
   };
-
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
