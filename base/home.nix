e5810b466d3721d37ffea89484a647058cde749d
muiga <muigask@gmail.com>
2024-11-07 15:54:24 +0300

    split all home configs 4

--- base/home.nix
+++ base/home.nix
@@ -29,8 +29,6 @@ let
 in
 {
 
-  home.username = "muiga";
-  home.homeDirectory = "/home/muiga";
   # Allow unfree packages
   #      nixpkgs.config.allowUnfree = true;
   # The home.packages option allows you to install Nix packages into your
