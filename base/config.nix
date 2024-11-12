dfe03597b1fb15405652078d15921a19be3b5190
muiga <muigask@gmail.com>
2024-11-12 10:53:25 +0300

    install usbutils and open-fprintd

--- base/config.nix
+++ base/config.nix
@@ -86,6 +86,12 @@
 	   xkb.variant = "";
     };
 
+    btrfs.autoScrub = {
+      enable = true;
+      interval = "monthly";
+      fileSystems = [ "/" ];
+    };
+
     # postgresql
     postgresql = {
       enable = true;
@@ -280,71 +286,73 @@
       fd
       efibootmgr
       zsh-powerlevel10k
-            firefox
-            kate
-            vlc
-            brave
-            inkscape-with-extensions
-            insomnia
-            haruna
-            discord
-            motrix
-            persepolis
-            onedrive
-            obs-studio
-            # easyeffects
-            nodejs_20
-            htop
-            gwenview
-            ferdium
-            libreoffice-qt
-            pdfarranger
-            winbox
-            todoist-electron
-            megasync
-            motrix
-            clipgrab
-            ffmpeg
-            yt-dlp
-            epsonscan2
-            libva
-            libva-utils
-            mpv
-            smplayer
-            filebot
-            anydesk
-            mellowplayer
-            tailscale
-            thunderbird-128
-            qbittorrent
-            shotcut
-            clementine
-            arduino
-            sleuthkit
-            bottom
-            microsoft-edge
-            spotify
-            telegram-desktop
-            appimage-run
-            bruno
-            vscode.fhs
-            protonvpn-gui
-            planify
-            ticktick
-            joplin-desktop
-            jetbrains.webstorm
-            jetbrains.rust-rover
-            gimp
-            super-productivity
-            nodePackages.prettier
-            slack
-            spotube
-            stremio
-            mailspring
-            codeium
-            dprint
-            google-chrome
-            jq
+      usbutils
+      open-fprintd
+      firefox
+      kate
+      vlc
+      brave
+      inkscape-with-extensions
+      insomnia
+      haruna
+      discord
+      motrix
+      persepolis
+      onedrive
+      obs-studio
+      # easyeffects
+      nodejs_20
+      htop
+      gwenview
+      ferdium
+      libreoffice-qt
+      pdfarranger
+      winbox
+      todoist-electron
+      megasync
+      motrix
+      clipgrab
+      ffmpeg
+      yt-dlp
+      epsonscan2
+      libva
+      libva-utils
+      mpv
+      smplayer
+      filebot
+      anydesk
+      mellowplayer
+      tailscale
+      thunderbird-128
+      qbittorrent
+      shotcut
+      clementine
+      arduino
+      sleuthkit
+      bottom
+      microsoft-edge
+      spotify
+      telegram-desktop
+      appimage-run
+      bruno
+      vscode.fhs
+      protonvpn-gui
+      planify
+      ticktick
+      joplin-desktop
+      jetbrains.webstorm
+      jetbrains.rust-rover
+      gimp
+      super-productivity
+      nodePackages.prettier
+      slack
+      spotube
+      stremio
+      mailspring
+      codeium
+      dprint
+      google-chrome
+      jq
     ];
     shells = with pkgs; [
       zsh
